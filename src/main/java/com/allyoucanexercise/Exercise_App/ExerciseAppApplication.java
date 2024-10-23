da856ea4cffb2839c7501dc0287e0b728003fddf
alishaburgfeld <98989927+alishaburgfeld@users.noreply.github.com>
2024-10-23 16:33:43 -0400

    removed reference to docker in pom xml and the compose yaml, as well as deleted the refrence to mysql and added the exclusion class to the application so my application would build successfully.

--- src/main/java/com/allyoucanexercise/Exercise_App/ExerciseAppApplication.java
+++ src/main/java/com/allyoucanexercise/Exercise_App/ExerciseAppApplication.java
@@ -1,13 +1,34 @@
 package com.allyoucanexercise.Exercise_App;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class ExerciseAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExerciseAppApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandlineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Lets inspect the beans provided by spring boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+		};
+	}
+
 }
+
+// guide: spring.io/guides/gs/spring-boot
