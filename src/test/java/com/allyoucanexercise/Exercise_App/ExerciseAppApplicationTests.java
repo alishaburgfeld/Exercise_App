bfff838384240ad440cbf45383bb0f2ac05c6e73
alishaburgfeld <98989927+alishaburgfeld@users.noreply.github.com>
2024-10-23 15:40:59 -0400

    initial springboot setup

---
+++ src/test/java/com/allyoucanexercise/Exercise_App/ExerciseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allyoucanexercise.Exercise_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
